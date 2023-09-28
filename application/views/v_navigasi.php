2209a57ee2ac162fea9ef208f8b12df66a0280d4
programeraa <145954791+programeraa@users.noreply.github.com>
2023-09-28 18:21:27 +0700

    Update v_navigasi.php
    tambahkan navigasi data pengguna

--- application/views/v_navigasi.php
+++ application/views/v_navigasi.php
@@ -1,18 +1,19 @@
-<div class="d-flex justify-content-start">
+<div class="d-flex bd-highlight mb-2">
     <div class="text-right">
         <a href="<?= base_url('Komisi');?>">
-            <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Dashboard</button></a>
+            <button type="button" class="btn btn-dark" data-whatever="@getbootstrap" >Dashboard</button></a>
         </div>
-        <!-- <div class="text-right ml-2">
-            <a href="<?= base_url('Karyawan');?>">
-                <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Karyawan</button></a>
-            </div> -->
+        <div class="text-right ml-2">
+            <a href="<?= base_url('Marketing');?>">
+                <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Marketing</button></a>
+            </div>
+
             <div class="text-right ml-2">
-                <a href="<?= base_url('Marketing');?>">
-                    <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Marketing</button></a>
+                <a href="<?= base_url('Pengguna');?>">
+                    <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Data Pengguna</button></a>
                 </div>
-        </div>
-        
+            </div>
+
             <div class="text-right">
                 <!-- <a href="<?php echo base_url('pengajuan/tambah_pengajuan') ?>"><button type="button" class="btn btn-primary"  data-whatever="@getbootstrap" >Tambah Data</button></a> -->
 
