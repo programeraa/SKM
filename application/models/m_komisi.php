03245751e17c32201dab93e3e3cbfd0deac75787
programeraa <145954791+programeraa@users.noreply.github.com>
2023-09-28 19:18:24 +0700

    Update m_komisi.php
    nambah relasi ke tabel pengguna

--- application/models/m_komisi.php
+++ application/models/m_komisi.php
@@ -9,9 +9,11 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi
+			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi,d.admin_pengguna,
 
-			FROM komisi as a INNER JOIN marketing as b ON a.mar_listing_komisi = b.id_mar INNER JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi ');
+			e.id_pengguna, e.nama_pengguna, e.level_pengguna
+
+			FROM komisi as a INNER JOIN marketing as b ON a.mar_listing_komisi = b.id_mar INNER JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna');
 		return $hasil;
 	}
 
@@ -27,9 +29,11 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi
+			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi,d.admin_pengguna,
 
-			FROM komisi as a INNER JOIN marketing as b ON a.mar_listing_komisi = b.id_mar INNER JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi WHERE a.id_komisi = '$data'");
+			e.id_pengguna, e.nama_pengguna, e.level_pengguna
+
+			FROM komisi as a INNER JOIN marketing as b ON a.mar_listing_komisi = b.id_mar INNER JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna WHERE a.id_komisi = '$data'");
 		return $hasil;
 	}
 
