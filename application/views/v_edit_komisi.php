ea758e140b348b0fd91b04ebcf32633109e7df75
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-03 14:07:37 +0700

    Update v_edit_komisi.php

--- application/views/v_edit_komisi.php
+++ application/views/v_edit_komisi.php
@@ -10,7 +10,59 @@
             <h4 class="card-title">Edit Data Komisi</h4>
         </div>
         <div class="card-body">
-            <?php foreach ($komisi as $komisi) { ?>
+            <?php foreach ($komisi as $komisi) { 
+
+                //setting apakah listing 1 a&a atau cobroke
+                foreach ($marketing as $mkt) {
+                    if ($mkt->id_mar == $komisi->mar_listing_komisi) {
+                        $listing_1 = $mkt->nama_mar;
+                        break;
+                    }else {
+                        foreach ($co_broke as $kubruk) {
+                            if ($komisi->mar_listing_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
+                                $listing_1 = $kubruk->nama_cobroke;
+                            }
+                        }
+                    }
+                }
+
+                //setting apakah selling 1 a&a atau cobroke
+                foreach ($marketing as $mkt) {
+                    if ($mkt->id_mar == $komisi->mar_selling_komisi) {
+                        $selling_1 = $mkt->nama_mar;
+                        break;
+                    }else{
+                        foreach ($co_broke as $kubruk) {
+                            if ($komisi->mar_selling_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
+                                $selling_1 = $kubruk->nama_cobroke;
+                            }
+                        }
+                    }
+                }
+
+                //setting marketing listing 2
+                if (!empty($komisi->mar_listing2_komisi)) {
+                    foreach ($marketing as $mkt) {
+                        if ($mkt->id_mar == $komisi->mar_listing2_komisi) {
+                            $listing2_baru = '- '.$mkt->nama_mar;
+                        }
+                    }
+                }else{
+                    $listing2_baru = '';
+                }
+
+                //setting marketing selling 2
+                if (!empty($komisi->mar_selling2_komisi)) {
+                    foreach ($marketing as $mkt) {
+                        if ($mkt->id_mar == $komisi->mar_selling2_komisi) {
+                            $selling2_baru = '- '.$mkt->nama_mar;
+                        }
+                    }
+                }else{
+                    $selling2_baru = '';
+                }
+
+                ?>
                 <form method="post" action="<?= base_url('komisi/update_komisi'); ?>">
                     <div class="row">
                         <div class="col-md-6">
@@ -42,7 +94,8 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="marketing_listing" class="col-form-label">Marketing Listing</label>
-                                <select class="form-control select2bs4" id="marketing_listing" name="marketing_listing">
+                                <input type="text" class="form-control" id="marketing_listing" name="marketing_listing" value="<?= $listing_1 ?> <?= $listing2_baru ?>" readonly>
+                                <!-- <select class="form-control select2bs4" id="marketing_listing" name="marketing_listing">
                                     <option value="">Pilih Nama</option>
                                     <?php 
                                     foreach($marketing as $each){ ?>
@@ -52,11 +105,12 @@
                                         </option>;
                                     <?php } ?>
                                 ?>
-                            </select>
+                            </select> -->
                         </div>
                         <div class="form-group">
                             <label for="marketing_selling" class="col-form-label">Marketing Selling</label>
-                            <select class="form-control select2bs4" id="marketing_selling" name="marketing_selling">
+                            <input type="text" class="form-control" id="marketing_selling" name="marketing_selling" value="<?= $selling_1 ?> <?= $selling2_baru ?>" readonly>
+                            <!-- <select class="form-control select2bs4" id="marketing_selling" name="marketing_selling">
                                 <option value="">Pilih Nama</option>
                                 <?php 
                                 foreach($marketing as $each){ ?>
@@ -66,7 +120,7 @@
                                     </option>;
                                 <?php } ?>
                             ?>
-                        </select>
+                        </select> -->
                     </div>
                     <div class="form-group">
                         <label for="komisi" class="col-form-label">Komisi Bruto</label>
