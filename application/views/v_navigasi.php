6e5a7bf174e39dc94f3fb9735567ec9561105383
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-06 10:31:38 +0700

    Update v_navigasi.php

--- application/views/v_navigasi.php
+++ application/views/v_navigasi.php
@@ -7,16 +7,17 @@
             <a href="<?= base_url('Marketing');?>">
                 <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Marketing</button></a>
             </div>
+            <?php if ($level == 'Administrator'): ?> 
+                <div class="text-right ml-2">
+                    <a href="<?= base_url('Pengguna');?>">
+                        <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Data Pengguna</button></a>
+                    </div>
+                <?php endif ?>
+            </div>
 
-            <div class="text-right ml-2">
-                <a href="<?= base_url('Pengguna');?>">
-                    <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Data Pengguna</button></a>
+            <?php if ($level == 'Administrator'): ?> 
+                <div class="text-right">
+                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" >Tambah Data</button>
                 </div>
-            </div>
-
-            <div class="text-right">
-                <!-- <a href="<?php echo base_url('pengajuan/tambah_pengajuan') ?>"><button type="button" class="btn btn-primary"  data-whatever="@getbootstrap" >Tambah Data</button></a> -->
-
-                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" >Tambah Data</button>
-            </div>
+            <?php endif ?>
 
