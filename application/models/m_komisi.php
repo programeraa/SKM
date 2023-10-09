63ea1f76c0c13afe402b868ffab6398e8be8fe12
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-09 11:32:40 +0700

    Update m_komisi.php

--- application/models/m_komisi.php
+++ application/models/m_komisi.php
@@ -3,7 +3,7 @@
 class M_komisi extends CI_Model{
 
 	function tampil_data(){
-		$hasil = $this->db->query('SELECT a.id_komisi, a.alamat_komisi, a.jt_komisi, a.tgl_closing_komisi, a.bruto_komisi, a.mar_listing_komisi, a.mar_selling_komisi, a.waktu_komisi,
+		$hasil = $this->db->query('SELECT a.id_komisi, a.alamat_komisi, a.jt_komisi, a.tgl_closing_komisi, a.bruto_komisi, a.mar_listing_komisi, a.mar_selling_komisi, a.waktu_komisi, a.status_komisi, a.tgl_disetujui,
 
 			a.mar_listing2_komisi, a.mar_selling2_komisi, 
 
@@ -11,7 +11,7 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi,d.admin_pengguna,
+			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
 
 			d.mm2_listing_komisi, d.npwpm2_listing_komisi, d.npwpum2_listing_komisi, d.npwpum2_listing2_komisi, d.mm2_selling_komisi, d.npwpm2_selling_komisi, d.npwpum2_selling_komisi, d.npwpum2_selling2_komisi,
 
@@ -19,13 +19,17 @@ class M_komisi extends CI_Model{
 
 			h.nama_mar as listing_2, h.norek_mar as norek_listing2, h.upline_emd_mar as up_1_listing2, h.upline_cmo_mar as up_2_listing2,
 
-			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2
+			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2,
+
+			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui
 
 			FROM komisi as a LEFT JOIN marketing as b ON a.mar_listing_komisi = b.id_mar LEFT JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna
 
 			LEFT JOIN marketing as h ON h.id_mar = a.mar_listing2_komisi
 
 			LEFT JOIN marketing as i ON i.id_mar = a.mar_selling2_komisi
+
+			LEFT JOIN pengguna as j ON j.id_pengguna = d.admin_status_komisi
 
 			');
 		return $hasil;
@@ -49,7 +53,7 @@ class M_komisi extends CI_Model{
 
 	function tampil_data_rincian($where){
 		$data = implode($where);
-		$hasil = $this->db->query("SELECT a.id_komisi, a.alamat_komisi, a.jt_komisi, a.tgl_closing_komisi, a.bruto_komisi, a.mar_listing_komisi, a.mar_selling_komisi, a.waktu_komisi,
+		$hasil = $this->db->query("SELECT a.id_komisi, a.alamat_komisi, a.jt_komisi, a.tgl_closing_komisi, a.bruto_komisi, a.mar_listing_komisi, a.mar_selling_komisi, a.waktu_komisi, a.status_komisi, a.tgl_disetujui,
 
 			a.mar_listing2_komisi, a.mar_selling2_komisi, 
 
@@ -57,7 +61,7 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi,d.admin_pengguna,
+			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
 
 			d.mm2_listing_komisi, d.npwpm2_listing_komisi, d.npwpum2_listing_komisi, d.npwpum2_listing2_komisi, d.mm2_selling_komisi, d.npwpm2_selling_komisi, d.npwpum2_selling_komisi, d.npwpum2_selling2_komisi,
 
@@ -65,13 +69,17 @@ class M_komisi extends CI_Model{
 
 			h.nama_mar as listing_2, h.norek_mar as norek_listing2, h.upline_emd_mar as up_1_listing2, h.upline_cmo_mar as up_2_listing2,
 
-			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2
+			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2,
+
+			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui
 
 			FROM komisi as a LEFT JOIN marketing as b ON a.mar_listing_komisi = b.id_mar LEFT JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna
 
 			LEFT JOIN marketing as h ON h.id_mar = a.mar_listing2_komisi
 
 			LEFT JOIN marketing as i ON i.id_mar = a.mar_selling2_komisi
+
+			LEFT JOIN pengguna as j ON j.id_pengguna = d.admin_status_komisi
 
 			WHERE a.id_komisi = '$data'");
 		return $hasil;
@@ -108,6 +116,11 @@ class M_komisi extends CI_Model{
 	function update($where,$data){
 		$this->db->where($where);
 		$this->db->update('komisi',$data);
+	}
+
+	function update_sub_komisi($where,$data2){
+		$this->db->where($where);
+		$this->db->update('sub_komisi',$data2);
 	}
 
 	function hapus($where){
