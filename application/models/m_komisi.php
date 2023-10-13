8776eea3d536c3dd88fb49c9e4576e8b82fc186a
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-13 09:40:13 +0700

    Update m_komisi.php

--- application/models/m_komisi.php
+++ application/models/m_komisi.php
@@ -11,7 +11,7 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
+			d.id_sub_komisi, d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
 
 			d.mm2_listing_komisi, d.npwpm2_listing_komisi, d.npwpum2_listing_komisi, d.npwpum2_listing2_komisi, d.mm2_selling_komisi, d.npwpm2_selling_komisi, d.npwpum2_selling_komisi, d.npwpum2_selling2_komisi,
 
@@ -21,7 +21,9 @@ class M_komisi extends CI_Model{
 
 			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2,
 
-			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui, j.gambar_ttd_pengguna
+			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui, j.gambar_ttd_pengguna,
+
+			k.id_afw, k.id_sub_komisi, k.m_ang, k.npwp_ang, k.npwp_up_ang, k.npwp_up2_ang, k.m_fran, k.npwp_fran, k.npwp_up_fran, k.npwp_up2_fran, k.m_win, k.npwp_win, k.npwp_up_win, k.npwp_up2_win 
 
 			FROM komisi as a LEFT JOIN marketing as b ON a.mar_listing_komisi = b.id_mar LEFT JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna
 
@@ -30,6 +32,10 @@ class M_komisi extends CI_Model{
 			LEFT JOIN marketing as i ON i.id_mar = a.mar_selling2_komisi
 
 			LEFT JOIN pengguna as j ON j.id_pengguna = d.admin_status_komisi
+
+			LEFT JOIN sub_komisi_afw as k ON k.id_sub_komisi = d.id_sub_komisi
+
+			ORDER BY a.id_komisi ASC
 
 			');
 		return $hasil;
@@ -61,7 +67,7 @@ class M_komisi extends CI_Model{
 
 			c.npwp_mar as npwp_selling, c.upline_emd_mar as up_1_selling, c.upline_cmo_mar as up_2_selling, c.nama_mar as nama_mar2, c.norek_mar as norek_selling,
 
-			d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
+			d.id_sub_komisi, d.id_komisi, d.mm_listing_komisi, d.npwpm_listing_komisi, d.npwpum_listing_komisi, d.npwpum_listing2_komisi, d.mm_selling_komisi, d.npwpm_selling_komisi, d.npwpum_selling_komisi, d.npwpum_selling2_komisi, d.admin_pengguna, d.admin_status_komisi,
 
 			d.mm2_listing_komisi, d.npwpm2_listing_komisi, d.npwpum2_listing_komisi, d.npwpum2_listing2_komisi, d.mm2_selling_komisi, d.npwpm2_selling_komisi, d.npwpum2_selling_komisi, d.npwpum2_selling2_komisi,
 
@@ -71,7 +77,9 @@ class M_komisi extends CI_Model{
 
 			i.nama_mar as selling_2, i.norek_mar as norek_selling2, i.upline_emd_mar as up_1_selling2, i.upline_cmo_mar as up_2_selling2,
 
-			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui, j.gambar_ttd_pengguna
+			j.id_pengguna, j.nama_pengguna as admin_disetujui, j.level_pengguna as level_disetujui, j.gambar_ttd_pengguna,
+
+			k.id_afw, k.id_sub_komisi, k.m_ang, k.npwp_ang, k.npwp_up_ang, k.npwp_up2_ang, k.m_fran, k.npwp_fran, k.npwp_up_fran, k.npwp_up2_fran, k.m_win, k.npwp_win, k.npwp_up_win, k.npwp_up2_win 
 
 			FROM komisi as a LEFT JOIN marketing as b ON a.mar_listing_komisi = b.id_mar LEFT JOIN marketing as c ON a.mar_selling_komisi = c.id_mar INNER JOIN sub_komisi as d ON a.id_komisi = d.id_komisi INNER JOIN pengguna as e ON e.id_pengguna = d.admin_pengguna
 
@@ -80,6 +88,8 @@ class M_komisi extends CI_Model{
 			LEFT JOIN marketing as i ON i.id_mar = a.mar_selling2_komisi
 
 			LEFT JOIN pengguna as j ON j.id_pengguna = d.admin_status_komisi
+
+			LEFT JOIN sub_komisi_afw as k ON k.id_sub_komisi = d.id_sub_komisi
 
 			WHERE a.id_komisi = '$data'");
 		return $hasil;
@@ -97,6 +107,10 @@ class M_komisi extends CI_Model{
 		$this->db->insert('sub_komisi',$data2);
 	}
 
+	function get_last_inserted_id_sub_komisi() {
+		return $this->db->insert_id();
+	}
+
 	function simpan_co_broke($data3){
 		$this->db->insert('co_broke',$data3);
 	}
@@ -107,6 +121,10 @@ class M_komisi extends CI_Model{
 
 	function simpan_referal($data5){
 		$this->db->insert('referal',$data5);
+	}
+
+	function simpan_sub_komisi_afw($data6){
+		$this->db->insert('sub_komisi_afw',$data6);
 	}
 
 	function edit($where){
