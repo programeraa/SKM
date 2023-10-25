f4e067827a5b0ad8007d53b077e5667e41f84f98
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-25 11:31:53 +0700

    Update v_dashboard.php

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -7,19 +7,19 @@
         </div>
         <div class="card">
             <div class="card-header bg-dark text-white">
-                <h4 class="card-title" style="text-align: center;">Dashboard Uji Coba</h4>
+                <h4 class="card-title" style="text-align: center;">Dashboard</h4>
             </div>
 
             <div class="card-body">
                 <div class="row mb-3">
-                    <div class="col-xl-3 col-sm-6 py-2">
+                    <div class="col-xl-3 col-sm-6 py-2 pt-0">
                         <div class="card bg-success text-white h-100">
                             <div class="card-body bg-success">
                                 <div class="rotate">
                                     <i class="fa fa-list fa-2x"></i>
                                 </div>
-                                <h6 class="text-uppercase">Data Pengajuan</h6>
-                                <h1 class="display-6"><?php //echo implode($data_pengajuan); ?></h1>
+                                <h6 class="text-uppercase">Data Komisi</h6>
+                                <h1 class="display-6"><?php echo implode($data_komisi); ?></h1>
                             </div>
                         </div>
                     </div>
@@ -30,7 +30,7 @@
                                     <i class="fa fa-check fa-2x"></i>
                                 </div>
                                 <h6 class="text-uppercase">Disetujui</h6>
-                                <h1 class="display-6"><?php //echo implode($disetujui); ?></h1>
+                                <h1 class="display-6"><?php echo implode($disetujui); ?></h1>
                             </div>
                         </div>
                     </div>
@@ -40,19 +40,19 @@
                                 <div class="rotate">
                                     <i class="fa fa-times fa-2x"></i>
                                 </div>
-                                <h6 class="text-uppercase">Ditolak</h6>
-                                <h1 class="display-6"><?php //echo implode($ditolak); ?></h1>
+                                <h6 class="text-uppercase">Belum Disetujui</h6>
+                                <h1 class="display-6"><?php echo implode($belum_disetujui); ?></h1>
                             </div>
                         </div>
                     </div>
                     <div class="col-xl-3 col-sm-6 py-2">
-                        <div class="card text-white bg-warning h-100">
-                            <div class="card-body bg-warning">
+                        <div class="card text-white bg-dark h-100">
+                            <div class="card-body bg-dark">
                                 <div class="rotate">
                                     <i class="fa fa-user fa-2x"></i>
                                 </div>
-                                <h6 class="text-uppercase">Jumlah Karyawan</h6>
-                                <h1 class="display-6"><?php //echo implode($karyawan); ?></h1>
+                                <h6 class="text-uppercase">Jumlah Marketing</h6>
+                                <h1 class="display-6"><?php echo implode($marketing); ?></h1>
                             </div>
                         </div>
                     </div>
