15b52ce25f06687c979fb8f5f60e8c6f5cd46ed7
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-25 11:31:27 +0700

    Update lihat_komisi.php

--- application/views/komisi/lihat_komisi.php
+++ application/views/komisi/lihat_komisi.php
@@ -1,6 +1,6 @@
 <!-- Modal Edit Data -->
 <div class="modal fade" id="lihat_komisi<?= $komisi->id_komisi?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Detail Data Komisi Marketing</h5>
