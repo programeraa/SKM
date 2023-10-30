5f6329ef85692060c51ecbd9daec118d67b62aa8
programeraa <145954791+programeraa@users.noreply.github.com>
2023-10-30 13:57:21 +0700

    Update v_navigasi.php

--- application/views/v_navigasi.php
+++ application/views/v_navigasi.php
@@ -5,19 +5,24 @@
         </div>
         <div class="text-right ml-2">
             <a href="<?= base_url('Marketing');?>">
-                <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Marketing</button></a>
+                <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Data Marketing</button></a>
             </div>
             <?php if ($level == 'Administrator'): ?> 
                 <div class="text-right ml-2">
                     <a href="<?= base_url('Pengguna');?>">
-                        <button type="button" class="btn btn-primary" data-whatever="@getbootstrap" >Data Pengguna</button></a>
+                        <button type="button" class="btn btn-success" data-whatever="@getbootstrap" >Data Pengguna</button></a>
+                    </div>
+
+                    <div class="text-right ml-2">
+                        <a href="<?= base_url('Pengaturan');?>">
+                            <button type="button" class="btn btn-info" data-whatever="@getbootstrap" >Pengaturan</button></a>
+                        </div>
+                    <?php endif ?>
+                </div>
+
+                <?php if ($level == 'Administrator'): ?> 
+                    <div class="text-right">
+                        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" >Tambah Data</button>
                     </div>
                 <?php endif ?>
-            </div>
-
-            <?php if ($level == 'Administrator'): ?> 
-                <div class="text-right">
-                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#exampleModal" data-whatever="@getbootstrap" >Tambah Data</button>
-                </div>
-            <?php endif ?>
 
