d928d4b4b0cc88dbb0e0fa2d97a69351fc152102
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-06 16:10:34 +0700

    Update v_status_approve.php

--- application/views/laporan/v_status_approve.php
+++ application/views/laporan/v_status_approve.php
@@ -5,87 +5,156 @@
     </div>
     <div class="card">
         <div class="card-header bg-dark text-white">
-            <h4 class="card-title" style="text-align: center;">Laporan Status Approve</h4>
+            <?php 
+            if(isset($_GET['dari']) && isset($_GET['ke'])){
+                $b = $_GET['dari'];
+                $t = $_GET['ke'];
+
+                if ($_GET['j_tanggal'] == 'tgl_closing_komisi') {
+                    $j = 'Tgl Closing';
+                }elseif($_GET['j_tanggal'] == 'waktu_komisi'){
+                    $j = 'Tgl Input';
+                }else{
+                    $j = 'Tgl Approve';
+                }
+            }else{
+                $b = null;
+                $t = null;
+                $j = null;
+            }
+            ?>
+
+            <?php if ($b == null) {?>
+                <h4 class="card-title" style="text-align: center;">Laporan Status Approve</h4>
+            <?php }else{ ?>
+                <h4 class="card-title" style="text-align: center;">Laporan Status Approve</h4>
+                <h5><?php echo '<div class="justify-content-center">
+                <div class="col-auto text-center">
+                <strong><span class= text-warning>'.$j.' Periode '.$b.' sampai '.$t.'</span></strong> 
+                </div>
+            </div>'; }?></h5> 
         </div>
 
         <div class="card-body">
-            <div class="table-responsive">
-                <table id="myTable" class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Alamat</th>
-                            <th>Jenis Transaksi</th>
-                            <th>Closing</th>
-                            <th>Marketing Listing</th>
-                            <th>Marketing Selling</th> 
-                            <th>Bruto</th>
-                            <th>Status</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php $no = 1; 
-                        foreach ($komisi as $komisi) { 
-                            //string to rupiah
-                            $bruto = stringToNumber($komisi->bruto_komisi);
-                            $bruto_r = numberToRupiah($bruto);
+            <div class="pb-3 d-sm-flex justify-content-start">
+              <form method="get" action="<?= base_url('laporan/filterData'); ?>">
+                <div class="row g-3 align-items-center justify-content-end">
+                  <div class="col-auto">
+                    <label class="col-form-label">Periode</label>
+                </div>
+                <div class="col-auto p-0">
+                    <input type="date" class="form-control" name="dari" required="">
+                </div>
+                <div class="col-auto">
+                    -
+                </div>
+                <div class="col-auto p-0">
+                    <input type="date" class="form-control" name="ke" required="">
+                </div>
+                <div class="col-auto pr-0">
+                    <select class="form-control" id="j_tanggal" name="j_tanggal" required>
+                        <option value="tgl_closing_komisi">Tanggal Closing</option>
+                        <option value="waktu_komisi">Tanggal Input</option>
+                        <option value="tgl_disetujui">Tanggal Disetujui</option>
+                    </select>
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-primary" type="submit">Tampilkan</button>
+                </div>
+            </div>
+        </form>
+    </div>
 
-                            $tgl_closing = date("d-m-Y", strtotime($komisi->tgl_closing_komisi));
+    <div class="table-responsive">
+        <table id="myTable" class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Alamat</th>
+                    <th>Jenis Transaksi</th>
+                    <th>Closing</th>
+                    <th>Marketing Listing</th>
+                    <th>Marketing Selling</th> 
+                    <th>Bruto</th>
+                    <th>Status</th>
+                    <th>Tanggal Input</th>
+                    <th>Tanggal Approve</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php $no = 1; 
+                foreach ($komisi as $komisi) { 
+                            //string to rupiah
+                    $bruto = stringToNumber($komisi->bruto_komisi);
+                    $bruto_r = numberToRupiah($bruto);
+
+                    $tgl_closing = date("d-m-Y", strtotime($komisi->tgl_closing_komisi));
 
                             //setting apakah listing 1 a&a atau cobroke
-                            $listing_1 = $komisi->nama_mar;
-                            foreach ($co_broke as $kubruk) {
-                                if ($komisi->mar_listing_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
-                                    $listing_1 = $kubruk->nama_cobroke;
-                                }
-                            }
+                    $listing_1 = $komisi->nama_mar;
+                    foreach ($co_broke as $kubruk) {
+                        if ($komisi->mar_listing_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
+                            $listing_1 = $kubruk->nama_cobroke;
+                        }
+                    }
 
                             //setting apakah selling 1 a&a atau cobroke
-                            $selling_1 = $komisi->nama_mar2;
-                            foreach ($co_broke as $kubruk) {
-                                if ($komisi->mar_selling_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
-                                    $selling_1 = $kubruk->nama_cobroke;
-                                }
-                            }
+                    $selling_1 = $komisi->nama_mar2;
+                    foreach ($co_broke as $kubruk) {
+                        if ($komisi->mar_selling_komisi == $kubruk->id_komisi_unik && $komisi->id_komisi == $kubruk->id_komisi ) {
+                            $selling_1 = $kubruk->nama_cobroke;
+                        }
+                    }
 
                             //setting listing 2 dan selling 2
-                            if (!empty($komisi->listing_2)) {
-                                $listing2_baru = '- '.$komisi->listing_2;
-                            }else{
-                                $listing2_baru = '';
-                            }
+                    if (!empty($komisi->listing_2)) {
+                        $listing2_baru = '- '.$komisi->listing_2;
+                    }else{
+                        $listing2_baru = '';
+                    }
 
-                            if (!empty($komisi->selling_2)) {
-                                $selling2_baru = '- '.$komisi->selling_2;
-                            }else{
-                                $selling2_baru = '';
-                            }
+                    if (!empty($komisi->selling_2)) {
+                        $selling2_baru = '- '.$komisi->selling_2;
+                    }else{
+                        $selling2_baru = '';
+                    }
 
+                    ?>
+                    <tr>
+                        <td><?= $no; ?></td>
+                        <td><?= $komisi->alamat_komisi ?></td>
+                        <td><?= $komisi->jt_komisi?></td>
+                        <td><?= $tgl_closing?></td>
+                        <td><?= $listing_1 ?> <?= $listing2_baru?> </td>
+                        <td><?= $selling_1 ?> <?= $selling2_baru?></td>
+                        <td><?= $bruto_r ?></td>
+                        <td>
+                            <?php 
+                            if ($komisi->status_komisi=='Proses Approve') {
+                                echo "<span class='badge badge-warning p-2'>Proses Approve</span>";
+                            }else{
+                                echo "<span class='badge badge-success p-2'>Approve</span>";
+                            }?>
+                        </td>
+                        <td>
+                            <?= $komisi->waktu_komisi; ?>
+                        </td>
+                        <td>
+                            <?php if ($komisi->tgl_disetujui == 0000-00-00){
+                                echo "Belum Approve";
+                            }else{
+                                echo $komisi->tgl_disetujui;
+                            }
                             ?>
-                            <tr>
-                                <td><?= $no; ?></td>
-                                <td><?= $komisi->alamat_komisi ?></td>
-                                <td><?= $komisi->jt_komisi?></td>
-                                <td><?= $tgl_closing?></td>
-                                <td><?= $listing_1 ?> <?= $listing2_baru?> </td>
-                                <td><?= $selling_1 ?> <?= $selling2_baru?></td>
-                                <td><?= $bruto_r ?></td>
-                                <td>
-                                    <?php 
-                                    if ($komisi->status_komisi=='Proses Approve') {
-                                        echo "<span class='badge badge-warning p-2'>Proses Approve</span>";
-                                    }else{
-                                        echo "<span class='badge badge-success p-2'>Approve</span>";
-                                    }?>
-                                </td>
-                            </tr>
-                            <?php $no++;
-                        } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
+                        </td>
+                    </tr>
+                    <?php $no++;
+                } ?>
+            </tbody>
+        </table>
     </div>
+</div>
+</div>
 </div>
 </body>
 
