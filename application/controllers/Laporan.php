c90f38500930ca4503c2b45fe318e01a8ebbdba3
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-06 13:38:20 +0700

    Create Laporan.php

---
+++ application/controllers/Laporan.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Laporan extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_komisi');
+		$this->load->model('m_dashboard');
+		$this->load->model('m_marketing');
+		$this->load->model('m_pengguna');
+	}
+
+	public function status_approve()
+	{
+		$level = $this->session->userdata('level');
+		if ($level == '') {
+			$this->session->set_flashdata('gagal','Anda Belum Login');
+			redirect(base_url('login'));
+		}
+
+		$data['title'] = 'Laporan Status Approve';
+		$data['komisi'] = $this->m_komisi->tampil_data()->result();
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_status_approve', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function status_transaksi()
+	{
+		$level = $this->session->userdata('level');
+		if ($level == '') {
+			$this->session->set_flashdata('gagal','Anda Belum Login');
+			redirect(base_url('login'));
+		}
+
+		$data['title'] = 'Laporan Status Transfer';
+		$data['komisi'] = $this->m_komisi->tampil_data()->result();
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_status_transfer', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function admin_komisi()
+	{
+		$level = $this->session->userdata('level');
+		if ($level == '') {
+			$this->session->set_flashdata('gagal','Anda Belum Login');
+			redirect(base_url('login'));
+		}
+
+		$data['title'] = 'Laporan Admin Komisi';
+		$data['komisi'] = $this->m_komisi->tampil_data()->result();
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+		$data['pengguna'] = $this->m_komisi->tampil_data()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_admin_komisi', $data);
+		$this->load->view('v_footer', $data);
+	}
+}
