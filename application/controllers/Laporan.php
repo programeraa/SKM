385298f9ad77e2d8f63617a410f968bffda6a781
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-07 13:27:17 +0700

    Update Laporan.php

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -9,21 +9,6 @@ class Laporan extends CI_Controller {
 		$this->load->model('m_pengguna');
 	}
 
-	public function filterData() {
-		$dari = $this->input->get('dari');
-		$ke = $this->input->get('ke');
-		$j_tanggal = $this->input->get('j_tanggal');
-
-		$data['title'] = 'Laporan Status Approve';
-		$data['komisi'] = $this->m_komisi->getDataByDateRange($dari, $ke, $j_tanggal);
-		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
-		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
-
-		$this->load->view('v_header', $data);
-		$this->load->view('laporan/v_status_approve', $data);
-		$this->load->view('v_footer', $data);
-	}
-
 	public function status_approve()
 	{
 		$level = $this->session->userdata('level');
@@ -34,6 +19,21 @@ class Laporan extends CI_Controller {
 
 		$data['title'] = 'Laporan Status Approve';
 		$data['komisi'] = $this->m_komisi->tampil_data()->result();
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_status_approve', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function filterDataStatusApprove() {
+		$dari = $this->input->get('dari');
+		$ke = $this->input->get('ke');
+		$j_tanggal = $this->input->get('j_tanggal');
+
+		$data['title'] = 'Laporan Status Approve';
+		$data['komisi'] = $this->m_komisi->getDataByDateRange($dari, $ke, $j_tanggal);
 		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
 		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
 
@@ -60,6 +60,21 @@ class Laporan extends CI_Controller {
 		$this->load->view('v_footer', $data);
 	}
 
+	public function filterDataStatusTransfer() {
+		$dari = $this->input->get('dari');
+		$ke = $this->input->get('ke');
+		$j_tanggal = $this->input->get('j_tanggal');
+
+		$data['title'] = 'Laporan Status Transfer';
+		$data['komisi'] = $this->m_komisi->getDataByDateRange($dari, $ke, $j_tanggal);
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_status_transfer', $data);
+		$this->load->view('v_footer', $data);
+	}
+
 	public function admin_komisi()
 	{
 		$level = $this->session->userdata('level');
@@ -72,7 +87,24 @@ class Laporan extends CI_Controller {
 		$data['komisi'] = $this->m_komisi->tampil_data()->result();
 		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
 		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
-		$data['pengguna'] = $this->m_komisi->tampil_data()->result();
+		$data['pengguna'] = $this->m_pengguna->tampil_data_admin()->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('laporan/v_admin_komisi', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function filterDataAdminKomisi() {
+		$dari = $this->input->get('dari');
+		$ke = $this->input->get('ke');
+		$j_tanggal = $this->input->get('j_tanggal');
+		$admin_komisi = $this->input->get('admin_komisi');
+
+		$data['title'] = 'Laporan Admin Komisi';
+		$data['komisi'] = $this->m_komisi->getDataByDateRange($dari, $ke, $j_tanggal, $admin_komisi);
+		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
+		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+		$data['pengguna'] = $this->m_pengguna->tampil_data_admin()->result();
 
 		$this->load->view('v_header', $data);
 		$this->load->view('laporan/v_admin_komisi', $data);
