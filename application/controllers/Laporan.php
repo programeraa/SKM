18c5b4ffacf5d00b7fc474d362bb62069c9ce7c0
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-08 14:43:19 +0700

    Update Laporan.php

--- application/controllers/Laporan.php
+++ application/controllers/Laporan.php
@@ -21,8 +21,9 @@ class Laporan extends CI_Controller {
 		$data['title'] = 'Laporan Omzet Vision';
 		$data['komisi'] = $this->m_komisi->tampil_data()->result();
 		$data['marketing'] = $this->m_komisi->tampil_data_marketing()->result();
-		$data['omzet_vision'] = $this->m_laporan->tampil_data_omzet_vision()->result();
+		$data['omzet'] = $this->m_laporan->tampil_data_omzet();
 		$data['co_broke'] = $this->m_komisi->tampil_data_cobroke()->result();
+		$data['omzet_vision'] = $this->m_laporan->tampil_data_omzet_vision()->result();
 
 		$this->load->view('v_header', $data);
 		$this->load->view('laporan/v_omzet_vision', $data);
