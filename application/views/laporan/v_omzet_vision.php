9808c76fe86354f91f0455ec688f4d28f02996a7
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-08 14:43:02 +0700

    Update v_omzet_vision.php

--- application/views/laporan/v_omzet_vision.php
+++ application/views/laporan/v_omzet_vision.php
@@ -110,7 +110,7 @@
                 </thead>
                 <tbody>
                     <?php $no = 1; 
-                    foreach ($omzet_vision as $vision) {
+                    foreach ($omzet as $vision) {
                         $fee_kantor = stringToNumber($vision->fee_kantor);
                         $fee_kantor_r = numberToRupiah($fee_kantor);
 
@@ -139,15 +139,17 @@
                                 $nama_mar = $mar->nama_mar;
                             }
                         }
+
+                        $tgl_closing = date("d-m-Y", strtotime($vision->tgl_closing_komisi));
                         ?>
                         <tr>
                             <td><?= $no; ?></td>
                             <td><?= $vision->id_komisi; ?></td>
-                            <td></td>
+                            <td><?= $tgl_closing; ?></td>
                             <td><?= $id_mar ?></td>
                             <td><?= $nama_mar ?></td>
-                            <td></td>
-                            <td></td>
+                            <td><?= $vision->alamat_komisi; ?></td>
+                            <td><?= $vision->jt_komisi; ?></td>
                             <td><?= $fee_kantor_r ?></td>
                             <td><?= $fee_marketing_r ?></td>
                             <td><?= $ptn_admin_r ?></td>
@@ -159,6 +161,117 @@
                     } ?>
                 </tbody>
             </table>
+<!-- 
+            <table id="myTable" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th rowspan="2">No</th>
+                        <th rowspan="2">ID Komisi</th>
+                        <th rowspan="2">Tanggal Closing</th>
+                        <th rowspan="2">ID Marketing</th>
+                        <th rowspan="2">Marketing</th>
+                        <th rowspan="2">Alamat Properti</th>
+                        <th rowspan="2">Jenis</th>
+                        <th colspan="2" class="text-center">Fee Bruto</th>
+                        <th colspan="2" class="text-center">Potongan</th>
+                        <th colspan="2" class="text-center">Netto</th>
+                    </tr>
+                    <tr>
+                        <th>A&A Vision</th>
+                        <th>Marketing</th>
+                        <th>ADM</th>
+                        <th>PPH 21</th>
+                        <th>A&A Vision</th>
+                        <th>Marketing</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $no = 1; 
+                    foreach ($omzet as $vision) {
+                        // $fee_kantor = stringToNumber($vision->fee_kantor);
+                        // $fee_kantor_r = numberToRupiah($fee_kantor);
+
+                        // $fee_marketing = stringToNumber($vision->fee_marketing);
+                        // $fee_marketing_r = numberToRupiah($fee_marketing);
+
+                        // $ptn_admin = stringToNumber($vision->ptn_admin);
+                        // $ptn_admin_r = numberToRupiah($ptn_admin);
+
+                        // $ptn_pph = stringToNumber($vision->ptn_pph);
+                        // $ptn_pph_r = numberToRupiah($ptn_pph);
+
+                        // $netto_marketing = stringToNumber($vision->netto_marketing);
+                        // $netto_marketing_r = numberToRupiah($netto_marketing);
+
+                        // $total_fee_vsn = $vision->fee_kantor + $vision->ptn_admin;
+
+                        // $total_fee_vision = stringToNumber($total_fee_vsn);
+                        // $total_fee_vision_r = numberToRupiah($total_fee_vision);
+
+                        $nama_mar = null;
+                        $id_mar = null;
+                        foreach ($marketing as $mar) {
+                            foreach ($omzet_vision as $vision_2) {
+                                if ($vision_2->id_marketing == $mar->id_mar) {
+                                    $id_mar = $mar->nomor_mar;
+                                    $nama_mar = $mar->nama_mar;
+                                }
+                            }
+                        }
+
+                        $tgl_closing = date("d-m-Y", strtotime($vision->tgl_closing_komisi));
+                        ?>
+                        <tr>
+                            <td><?= $no; ?></td>
+                            <td><?= $vision->id_komisi; ?></td>
+                            <td><?= $tgl_closing; ?></td>
+                            <td>
+                                <table id="myTable" class="table table-bordered table-striped">
+                                    <tbody>
+                                        <tr>
+                                            <td>Sat</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sat</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Sat</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td>
+                                <table id="myTable" class="table table-bordered table-striped">
+                                    <tbody>
+                                        <?php 
+                                        foreach ($omzet_vision as $vision_2) {
+                                            if ($vision->id_omzet == $vision_2->id_omzet) {
+                                                foreach ($marketing as $mar) {
+                                                    if ($mar->id_mar == $vision_2->id_marketing) {
+                                                        echo '<tr>';
+                                                        echo '<td>'.$mar->nama_mar . '</td>';
+                                                        echo '</tr>';
+                                                    }
+                                                }
+                                            }
+                                        }
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </td>
+                            <td><?= $vision->alamat_komisi ?></td>
+                            <td><?= $vision->jt_komisi?></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                        <?php $no++;
+                    } ?>
+                </tbody>
+            </table> -->
         </div>
     </div>
 </div>
