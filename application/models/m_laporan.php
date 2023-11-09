fda4d6c2404f2514046de7cc19ae380ece91b581
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-09 16:08:34 +0700

    Update m_laporan.php

--- application/models/m_laporan.php
+++ application/models/m_laporan.php
@@ -7,6 +7,8 @@ class M_laporan extends CI_Model{
         $this->db->from('omzet');
         $this->db->join('komisi','komisi.id_komisi = omzet.id_komisi','inner');
         $this->db->join('omzet_aavision','omzet.id_omzet = omzet_aavision.id_omzet','inner');
+        
+        $this->db->order_by('omzet_aavision.id_omzetvision', 'ASC');
 
         $query = $this->db->get();
         return $query->result();
@@ -24,7 +26,6 @@ class M_laporan extends CI_Model{
         $query = $this->db->get();
         return $query->result();
     }
-
 
     function tampil_data_omzet_vision(){
         return $this->db->get('omzet_aavision');
@@ -58,4 +59,15 @@ class M_laporan extends CI_Model{
     function simpan4($data4){
         $this->db->insert('omzet_aavision',$data4);
     }
+
+    function simpan_ang($data_ang){
+        $this->db->insert('omzet_aavision',$data_ang);
+    }
+    function simpan_fran($data_fran){
+        $this->db->insert('omzet_aavision',$data_fran);
+    }
+    function simpan_win($data_win){
+        $this->db->insert('omzet_aavision',$data_win);
+    }
+
 }
