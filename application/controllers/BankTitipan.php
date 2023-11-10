ce49c7b87a9e97c1b5e98246c982bc1b7a3ce221
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-10 16:01:12 +0700

    Create BankTitipan.php

---
+++ application/controllers/BankTitipan.php
@@ -0,0 +1,139 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class BankTitipan extends CI_Controller {
+	function __construct(){
+		parent::__construct();
+		$this->load->model('m_marketing');	
+		$this->load->model('m_banktitipan');
+	}
+
+	public function index(){
+		$level = $this->session->userdata('level');
+		if ($level == '') {
+			$this->session->set_flashdata('gagal','Anda Belum Login');
+			redirect(base_url('login'));
+		}
+
+		$data['title'] = "Data Bank Titipan";
+		$data['marketing'] = $this->m_marketing->tampil_data()->result();
+		$data['bank_titipan'] = $this->m_banktitipan->tampil_data();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('v_banktitipan', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function tambah(){
+		$kode_perkiraan = $this->input->post('kode_perkiraan');
+		$nama_properti = $this->input->post('nama_properti');
+		$jt = $this->input->post('jt');
+		$tgl_input = $this->input->post('tgl_input');
+		$nama_marketing = $this->input->post('nama_marketing');
+		$nominal = $this->input->post('nominal');
+		$keterangan = $this->input->post('keterangan');
+
+		$data = array(
+			'kode_perkiraan' => $kode_perkiraan,
+			'nama_properti' => $nama_properti,
+			'status_properti' => $jt,
+			'id_marketing' => $nama_marketing,
+			'tgl_input' => $tgl_input,
+			'nilai_nominal' => $nominal,
+			'keterangan' => $keterangan
+		);
+
+		$this->m_banktitipan->simpan($data);
+
+		echo '<script>
+		alert("Selamat! Berhasil Menambah Data Bank Titipan");
+		window.location="' . base_url('BankTitipan') . '"
+		</script>';
+	}
+
+	public function edit($id){
+		$level = $this->session->userdata('level');
+		if ($level == '') {
+			$this->session->set_flashdata('gagal','Anda Belum Login');
+			redirect(base_url('login'));
+		}
+
+		$where = array('id_bta'=>$id);
+		$where_2 = array('id_bta'=>$id);
+		$data['title'] = "Edit Bank Titipan";
+		$data['marketing'] = $this->m_marketing->tampil_data()->result();
+		$data['bank_titipan'] = $this->m_banktitipan->tampil_data($where);
+		$data['kredit_bt'] = $this->m_banktitipan->tampil_data_kredit($where_2)->result();
+
+		$this->load->view('v_header', $data);
+		$this->load->view('v_edit_banktitipan', $data);
+		$this->load->view('v_footer', $data);
+	}
+
+	public function update(){
+		$kode_perkiraan = $this->input->post('kode_perkiraan');
+		$nama_properti = $this->input->post('nama_properti');
+		$jt = $this->input->post('jt');
+		$tgl_input = $this->input->post('tgl_input');
+		$nama_marketing = $this->input->post('nama_marketing');
+		$nominal = $this->input->post('nominal');
+		$keterangan = $this->input->post('keterangan');
+		$id = $this->input->post('id_bta');
+
+		$kredit = $this->input->post('kredit');
+		$n_kredit = $this->input->post('n_kredit');
+
+		date_default_timezone_set("Asia/Jakarta");
+		$waktu = date("Y-m-d H:i:s");
+
+		$data = array(
+			'kode_perkiraan' => $kode_perkiraan,
+			'nama_properti' => $nama_properti,
+			'status_properti' => $jt,
+			'id_marketing' => $nama_marketing,
+			'tgl_input' => $tgl_input,
+			'nilai_nominal' => $nominal,
+			'keterangan' => $keterangan
+		);
+
+		$data_kredit = array(
+			'id_bta' => $id,
+			'tgl_input_kredit' => $waktu,
+			'keterangan_kredit' => $kredit,
+			'nominal_kredit' => $n_kredit
+		);
+
+
+		$where = array('id_bta'=>$id);
+
+		if (isset($where)) {
+			$eksekusi = $this->m_banktitipan->update($where,$data);
+			if (!empty($kredit)) {
+				$this->m_banktitipan->simpan_kredit($data_kredit);
+			}
+			echo '<script>
+			alert("Selamat! Berhasil Update Data BankTitipan");
+			window.location="' . base_url('BankTitipan') . '"
+			</script>';
+		}
+		echo '<script>
+		alert("Gagal Update Data BankTitipan");
+		window.location="' . base_url('BankTitipan') . '"
+		</script>';
+	}
+
+	public function hapus($id_bta){
+		$where = array('id_bta'=>$id_bta);
+		if (isset($where)) {
+			$this->m_banktitipan->hapus($where);
+			echo '<script>
+			alert("Selamat! Data berhasil terhapus.");
+			window.location="' . base_url('BankTitipan') . '"
+			</script>';
+		} else {
+			echo '<script>
+			alert("Gagal Menghapus !, ID Pengguna ' . $id_bta . ' Tidak ditemukan");
+			window.location="' . base_url('BankTitipan') . '"
+			</script>';
+		}
+	}
+}
