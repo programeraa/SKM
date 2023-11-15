1d1c29cd8821d9126b7dae7be30e75f2fd875c20
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-15 16:04:52 +0700

    Update v_banktitipan.php

--- application/views/v_banktitipan.php
+++ application/views/v_banktitipan.php
@@ -72,18 +72,24 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php $no = 1; 
+                    <?php 
+                    $no = 1; 
                     $sum_debit = 0;
                     $sum_kredit = 0;
                     $sum_saldo_akhir = 0;
+                    $a = 0;
+                    $fix_banget = 0;
+                    $ttsa = 0;
 
                     $total_debit_r = null;
                     $total_kredit_r = null;
+                    $total_saldo_akhir = null;
                     $total_saldo_akhir_r = null;
 
-                    $id_komisi_values = [];
+                    $saldo_awalX = [];
 
                     foreach ($bank_titipan as $bt) {
+
                         $sum_kredit_per_baris = 0;
                         $ada_kredit = 0;
 
@@ -112,60 +118,81 @@
                         $total_debit = stringToNumber($sum_debit);
                         $total_debit_r = numberToRupiah($total_debit);
 
-                        // $total_debit = stringToNumber($sum_debit += $bt->nilai_nominal);
-                        // $total_debit_r = numberToRupiah($total_debit);
-
                         $total_kredit = stringToNumber($sum_kredit += $sum_kredit_per_baris + $nilai_kredit);
                         $total_kredit_r = numberToRupiah($total_kredit);
+
+                        $tgl_input = date("d-m-Y", strtotime($bt->tgl_input));
+                        $tgl_input_2 = date("m-Y", strtotime($bt->tgl_input));
 
                         $cell_content = '';
                         $saldo_awal_per_bulan = [];
                         
                         foreach ($tutup_jurnal as $jurnal) {
                             $tgl_input_jurnal = date("m-Y", strtotime($jurnal->tgl_jurnal));
-                            $saldo_awal_per_bulan[$tgl_input_jurnal] = $jurnal->saldo_akhir;
 
-                            if (!in_array($jurnal->saldo_akhir, $id_komisi_values)) {
-                                $cell_content = $jurnal->saldo_akhir;
-                                $id_komisi_values[] = $jurnal->saldo_akhir; 
+                            if ($tgl_input_2 == $tgl_input_jurnal) {
+                                $tgl_input_jurnal_2 = date("m-Y", strtotime($jurnal->tgl_jurnal));
+
+                                $saldo_awal_per_bulan[$tgl_input_jurnal] = $jurnal->saldo_akhir;
+
+                                if (!isset($saldo_awalX[$tgl_input_jurnal]) && !empty($jurnal->saldo_akhir)) {
+                                    $cell_content = $jurnal->saldo_akhir;
+                                    $saldo_awalX[$tgl_input_jurnal] = true; 
+                                }
                             }
                         }
 
                         $saldo_awal = stringToNumber($cell_content);
                         $saldo_awal_r = numberToRupiah($saldo_awal);
 
-                        if ($cell_content != 0) {
-                            $a = $cell_content;
-                        }
+                        // if ($saldo_awal != 0 && !isset($a)) {
+                        //     $a += $saldo_awal;
+                        // }elseif($saldo_awal != 0){
+                        //     $a += $saldo_awal;
+                        // }elseif(!isset($a)){
+                        //     $a += $saldo_awal;
+                        // }
 
-                        $fix_saldo_akhir = $sum_saldo_akhir += $saldo_akhir;
+                        // $fix_saldo_akhir = $sum_saldo_akhir += $saldo_akhir;
 
-                        $total_saldo_akhir = stringToNumber($fix_saldo_akhir + $a);
-                        $total_saldo_akhir_r = numberToRupiah($total_saldo_akhir);
+                        // $total_saldo_akhir = stringToNumber($fix_saldo_akhir + $a);
+                        // $total_saldo_akhir_r = numberToRupiah($total_saldo_akhir);
 
                         $nominal = stringToNumber($bt->nilai_nominal);
                         $nominal_r = numberToRupiah($nominal);
 
-                        $tgl_input = date("m-Y", strtotime($bt->tgl_input));
-
-                        if (array_key_exists($tgl_input, $saldo_awal_per_bulan)) {
-                            $saldo_awal = $saldo_awal_per_bulan[$tgl_input];
-                        } else {
-                            $saldo_awal = 0;
-                        }
-                        $saldo_awal_r = numberToRupiah($saldo_awal);
-
-
                         $nama_mar = null;
                         foreach ($marketing as $mar) {
                             if ($mar->id_mar == $bt->id_marketing) {
-                             $nama_mar = $mar->nama_mar;
-                             break;
-                         }
-                     }
+                               $nama_mar = $mar->nama_mar;
+                               break;
+                           }
+                       }
 
-                     ?>
-                     <tr>
+                       if (!empty($saldo_awal)) {
+                        if ($bt->jenis == 'Kredit') {
+                            if ($ada_kredit != 0) {
+                                $final = $saldo_awal - ($bt->nilai_nominal + $sum_kredit_per_baris);
+                            }else{
+                                $final = $saldo_awal - $saldo_akhir;
+                            }
+                        }else{
+                            $final = $saldo_akhir + $saldo_awal;
+                        }
+                    }else{
+                        if ($ada_kredit != 0) {
+                            $final = $nominal + $ada_kredit;
+                        }else{
+                            $final = $saldo_akhir;
+                        }
+                    }
+
+                    $tsa = $fix_banget += $final;
+
+                    $total_saldo_awal = $ttsa += $saldo_awal;
+
+                    ?>
+                    <tr>
                         <td><?= $no;?></td>
                         <td><?= $bt->kode_perkiraan ?></td>
                         <td><?= $bt->nama_properti ?></td>
@@ -174,9 +201,9 @@
                         <td><?= $tgl_input ?></td>
                         <td>
                             <?php
-                            if ($saldo_awal_r != 0) {
+                            if (!empty($cell_content)) {
                                 echo $saldo_awal_r;
-                            }else{
+                            } else {
                                 echo '';
                             } 
                             ?> 
@@ -196,9 +223,9 @@
                                 if ($ada_kredit != 0) {
                                     echo numberToRupiah($akhir = $nominal + $ada_kredit);
                                 }else{
-                                 echo $nominal_r;
-                             }
-                         }else{
+                                   echo $nominal_r;
+                               }
+                           }else{
                             echo '';
                         }
                     }else{
@@ -210,13 +237,7 @@
                     }
                     ?>
                 </td>
-                <td><?php
-                if (!empty($saldo_awal)) {
-                    $fix = $saldo_akhir + $saldo_awal;
-                    echo numberToRupiah($fix);
-                }else{
-                    echo $saldo_akhir_r; 
-                }?>
+                <td><?= numberToRupiah($final);?>
             </td>
         </tr>
         <?php $no++;
@@ -230,16 +251,16 @@
         <th></th>
         <th></th>
         <th></th>
-        <th></th>
+        <th><?= numberToRupiah($total_saldo_awal); ?></th>
         <th><?= $total_debit_r ?></th>
         <th><?= $total_kredit_r ?></th>
-        <th><?= $total_saldo_akhir_r ?></th>
+        <th><?= numberToRupiah($tsa) ?></th>
     </tr>
 </tfoot>
 </table>
 </div>
 <div class="text-right mt-3">
-    <a href="<?= base_url('BankTitipan/tutup_jurnal?tsa=' . $total_saldo_akhir.'&bulan='.$nama_bulan); ?>" onclick="javascript:return confirm('Apakah Anda yakin ingin tutup jurnal bulan (<?= $nama_bulan ?>) ?')" class="btn btn-danger mt-1">Tutup Jurnal (<?= $nama_bulan ?>)</a>
+    <a href="<?= base_url('BankTitipan/tutup_jurnal?tsa=' . $tsa.'&bulan='.$nama_bulan); ?>" onclick="javascript:return confirm('Apakah Anda yakin ingin tutup jurnal bulan (<?= $nama_bulan ?>) ?')" class="btn btn-danger mt-1">Tutup Jurnal (<?= $nama_bulan ?>)</a>
 
 </div>
 </div>
