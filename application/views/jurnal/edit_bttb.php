00bba265c8e5ef83b282adfb64e3692e70d6d15f
programeraa <145954791+programeraa@users.noreply.github.com>
2023-11-25 13:50:07 +0700

    Update edit_bttb.php

--- application/views/jurnal/edit_bttb.php
+++ application/views/jurnal/edit_bttb.php
@@ -3,16 +3,26 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Data BT / TB</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Edit Data Akun</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
             <div class="modal-body">
                 <form method="post" action="<?= base_url('Jurnal/update_bttb'); ?>" enctype="multipart/form-data">
-                    <div class="form-group">
-                        <label for="kode_perkiraan" class="col-form-label">Kode Perkiraaan</label>
-                        <input type="text" class="form-control" id="kode_perkiraan" name="kode_perkiraan" placeholder="Contoh : BT001 Atau TB001" value="<?= $jurnal->kode_perkiraan ?>" required>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="kode_perkiraan" class="col-form-label">Kode Perkiraaan</label>
+                                <input type="text" class="form-control" id="kode_perkiraan" name="kode_perkiraan" placeholder="Contoh : BT001 Atau TB001" value="<?= $jurnal->kode_perkiraan ?>" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="nomor_perkiraan" class="col-form-label">Nomor Perkiraaan</label>
+                                <input type="text" class="form-control" id="nomor_perkiraan" name="nomor_perkiraan" placeholder="Contoh : 001, 002 Dst" value="<?= $jurnal->nomor_perkiraan ?>" required>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
                         <label for="keterangan" class="col-form-label">Keterangan</label>
