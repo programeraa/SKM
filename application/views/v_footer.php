c49f11a9be3129d7cc938ecc5fbe350ca1b502a7
programeraa <145954791+programeraa@users.noreply.github.com>
2024-01-22 10:29:16 +0700

    Update v_footer.php

--- application/views/v_footer.php
+++ application/views/v_footer.php
@@ -3,10 +3,11 @@
 date_default_timezone_set("Asia/Jakarta");
 $tahun = date("Y");
 ?>
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-	<div class="container d-flex justify-content-center">
-		<a class="navbar-brand" href="#">&copy; Copyright A&A INDONESIA <?= $tahun ?></a>
-	</div>
-</nav>
-
+<footer>
+	<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+		<div class="container d-flex justify-content-center">
+			<a class="navbar-brand" href="#">&copy; Copyright A&A INDONESIA <?= $tahun ?></a>
+		</div>
+	</nav>
+</footer>
 </html>
