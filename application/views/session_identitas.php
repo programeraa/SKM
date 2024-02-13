e1662534c5872952c05947ca9ccf0fa79be4364c
programeraa <145954791+programeraa@users.noreply.github.com>
2024-02-13 13:52:28 +0700

    Delete application/views directory

--- application/views/session_identitas.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$id = $this->session->userdata('id');
-$level = $this->session->userdata('level');
-$nama = $this->session->userdata('nama');
-$level_asli = $this->session->userdata('level_asli');
-$akses_level = $this->session->userdata('level_akses');
-?>
